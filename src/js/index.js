bad75411ebe505182c85598f7e619d52096bb537
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-03-02 10:10:23 +0100

    first commit react app

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../index.css';
+
+const app = document.querySelector('#app');
+
+ReactDOM.render(
+    <>
+    </>,
+    app
+)
