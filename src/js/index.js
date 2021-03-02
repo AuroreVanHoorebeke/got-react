afde26a59ee6d94f9e96988e2b9e431f072c4ca5
Aurore Van Hoorebeke <73104747+AuroreVanHoorebeke@users.noreply.github.com>
2021-03-02 11:59:00 +0100

    test deployment working api call

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../index.css';
 
+import ContentAPI from './components/ContentAPI.jsx'
+
 const app = document.querySelector('#app');
 
 ReactDOM.render(
     <>
+        <ContentAPI />
     </>,
     app
 )
